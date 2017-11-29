70ba1e677ef1ab815bc9d7a85a18a5e47da50527
numeroteca <pablo@basurama.org>
2017-11-29 13:49:59 +0100

    añade diagramas de flechas: extranjero y becas comedor

---
+++ mapa/flechas_becas-comedor.js
@@ -0,0 +1,396 @@
+// cartograma basado en original de https://martingonzalez.net/
+
+//Prepare canvas size
+isMobile = innerWidth < 758;
+
+var screenwidth = d3.select("#cartogram").node().clientWidth;
+
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+    //width = (isMobile ? (screenwidth+100) : screenwidth) - margin.left - margin.right,
+    width = 1100 - margin.left - margin.right,
+    ratio = 1,
+    height = width*ratio - margin.top - margin.bottom,
+    padding = 3;
+var square = 70
+
+// Rectangle size
+//must calculate manually the relationship of the squares of this values to match the min and max value ofthe domains
+// first value is the minimum size of square side, and the second the maximun size of square side
+var rectSize = d3.scaleSqrt()
+    .range([5, 100])
+
+// Line size
+var lineSize = d3.scaleLinear()
+		.domain([0,72])
+    .range([0,square])
+
+// Font size scale
+var fontSize = d3.scaleLinear()
+    .range([2,72.94])
+
+// Color
+var colorPub = d3.scaleLinear()
+    .domain([1, 40])
+    .range(['#fff','red'])
+var colorPriv = d3.scaleLinear()
+    .domain([1, 40])
+    .range(['#fff','blue'])
+
+// Adds cartogram svg
+var svg = d3.select("#cartogram").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate("+ margin.left +"," + margin.top + ")");
+
+//Adds Background image
+var background = svg.append('g').attr('id','backgroundimage');
+background.append("image")
+	.attr("xlink:href", "../images/leyenda-flechas-segregacion-extranjeros-red-pub-priv-euskadi.png")
+	.attr("x", width-400)
+	.attr("y", -80)
+	.attr("width", "350")
+	.attr("height", "311");
+
+// Adds arrows
+defs = svg.append("defs");
+
+defs.append("marker")
+	.attr("id","arrow")
+	.attr("viewBox","0 -5 10 10")
+	.attr("refX",5)
+	.attr("refY",0)
+	.attr("markerWidth",4)
+	.attr("markerHeight",4)
+	.attr("orient","auto")
+	.append("path")
+		.attr("d", "M0,-5L5,0L0,5")
+		.attr("class","arrowHead")
+		.attr("fill-opacity","0")
+		.attr("stroke","grey")
+
+defs.append("marker")
+	.attr("id","markerCircle")
+	.attr("refX",2)
+	.attr("refY",2)
+	.attr("markerWidth",4)
+	.attr("markerHeight",4)
+	.attr("orient","auto")
+	.append("circle")
+		.attr("class","circleHead")
+		.attr("fill-opacity","1")
+		.attr("fill","#bd0017")
+		.attr("stroke","none")
+		.attr("cx",2)
+		.attr("cy",2)
+		.attr("r",0.8)
+
+defs.append("marker")
+	.attr("id","markerCirclePriv")
+	.attr("refX",2)
+	.attr("refY",2)
+	.attr("markerWidth",4)
+	.attr("markerHeight",4)
+	.attr("orient","auto")
+	.append("circle")
+		.attr("class","circleHead")
+		.attr("fill-opacity","1")
+		.attr("fill","#00F")
+		.attr("stroke","none")
+		.attr("cx",2)
+		.attr("cy",2)
+		.attr("r",0.8)
+
+var rectangulos = svg.append('g').attr('id','rectangulos');
+var rectangulos2 = svg.append('g').attr('id','rectangulos2');
+var flechas = svg.append('g').attr('id','flechas');
+
+// Adds tooltip
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+
+d3.json("limites-zonas-escolares-euskadi-con-variables-2014-15_simplify2.json", function(err, data) {
+
+// move projection inside json to be able to get data
+var projection = d3.geoMercator().fitSize([width, height], topojson.feature(data, data.objects.barrios));
+
+var path = d3.geoPath()
+    .projection(projection);
+
+    // 1. Features we are painting
+    barrio = topojson.feature(data, data.objects.barrios).features
+
+    // Rect size scale
+    rectSize.domain(d3.extent(barrio, function(d) {return d.properties.total_alumnado }))
+
+    // Line size scale
+    // lineSize.domain(d3.extent(barrio, function(d) {return d.properties.total_alumnado })) TODO, as there is no "dif" variable for extranjeros
+
+    // 2. Create on each feature the centroid and the positions
+    barrio.forEach(function(d) {
+        d.pos = projection(d3.geoCentroid(d))
+        d.x = d.pos[0]
+        d.y = d.pos[1]
+        // Select how to scale the squares. Try and decide
+       // d.area = rectSize(d.properties.total_alumnado) / 0.75
+        d.area = square / 1.1
+        //d.lsize = lineSize(Math.abs(d.properties.perc_alum_ext_publi - d.properties.perc_alum_ext_publi)) no se usa
+    })
+
+    // Font size scale
+    fontSize.domain(d3.extent(barrio, function(d) {return d.area }))
+
+    // 3. Collide force
+    var simulation = d3.forceSimulation(barrio)
+        .force("x", d3.forceX(function(d) { return d.pos[0] }).strength(.1))
+        .force("y", d3.forceY(function(d) { return d.pos[1] }).strength(.1))
+        .force("collide", collide)
+
+    // 4. Number of simulations
+    for (var i = 0; i < 100; ++i) simulation.tick()
+
+    // 5. Paint the cartogram
+    var rect = rectangulos.selectAll("g")
+        .data(barrio)
+        .enter()
+        .append("g")
+        .attr("class", function(d) { return "zona " + d.properties.zona + " z" + d.properties.zona_id })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")" })
+          .on("mousemove", showTooltip) // AÑADIR EVENTO SHOW TOOLTIP
+					.on("mouseout", hideTooltip) // OCULTAR TOOLTIP
+
+/*    var rect2 = rectangulos2.selectAll("g")
+        .data(barrio)
+        .enter()
+        .append("g")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")" })
+          .on("mousemove", showTooltip) // AÑADIR EVENTO SHOW TOOLTIP
+					.on("mouseout", hideTooltip) // OCULTAR TOOLTIP
+*/		
+		var arrows = flechas.selectAll("g")
+        .data(barrio)
+        .enter()
+        .append("g")
+        .attr("transform", function(d) { return "translate(" + (d.x - square/2) + "," + d.y + ")" })
+          .on("mousemove", showTooltip) // AÑADIR EVENTO SHOW TOOLTIP
+					.on("mouseout", hideTooltip) // OCULTAR TOOLTIP
+
+		arrows.append("line")
+		  .each(function(d) {
+		  		var desigualdad = (d.properties.indice_desigualdad == null ) ? 0 : d.properties.indice_desigualdad;
+					var value = lineSize(Math.abs(d.properties.perc_bec_comedor_priv));
+		  		if ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) {
+		  			value = 0;
+		  		}
+		      d3.select(this)
+		        .attr("width", square)
+		        .attr("height", square)
+		        .attr("x1", lineSize(d.properties.perc_bec_comedor_pub))
+		        .attr("y1", 25)
+		        .attr("x2", value)
+		        .attr("y2", 25)
+		        .attr("class", Math.abs(d.properties.perc_bec_comedor_pub - d.properties.perc_bec_comedor_priv))
+		        .attr("marker-end", function(d) {
+				      if ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) {
+							endMarker = "";
+							} else { endMarker = "url(#markerCirclePriv)" }
+				      return endMarker;
+				      }
+		        )
+		        .attr("marker-start","url(#markerCircle)")
+		        .attr("fill","#bd0017")
+		        .attr("stroke-opacity","0.7")
+		        .attr("stroke", function(d) {
+		          var desigualdad = (d.properties.indice_desigualdad == null ) ? "--" : d.properties.indice_desigualdad;
+							color = "#000";
+							if  ( desigualdad == "--" ) {
+								desigualdad = "--";
+								color = "#FFF";
+							} else if ( desigualdad > 1 ) {
+								desigualdad = d.properties.indice_desigualdad;
+								color = "#F00";
+							} else {
+								desigualdad = d.properties.indice_desigualdad;
+								color = "#00F";
+							}
+		      		return color;
+		        })
+		        .attr("stroke-width", 4)
+		      })
+
+  rect.append("rect")
+        .each(function(d) {
+            d3.select(this)
+              .attr("width", d.area)
+              .attr("height", d.area)
+              .attr("x", -square / 2)
+              .attr("y", -square / 2)
+              //.attr("x", -d.area / 2)
+              //.attr("y", -d.area / 2)
+              /*.attr("fill", function(d) {
+		            if  ( d.properties.indice_desigualdad == null)  {
+									return "#CCC";
+								} else {
+									return colorPub(d.properties.perc_alum_ext_publi)
+								}
+							})*/
+							.attr("fill", "#eee")
+              .attr("stroke", "#fff")
+              .attr("stroke-width", 1)
+              .attr("rx", 0.5)
+          })
+
+/*	    rect2.append("rect")
+        .each(function(d) {
+            d3.select(this)
+              .attr("width", d.area)
+              .attr("height", d.area)
+              .attr("x", -d.area / 2)
+              .attr("y", -d.area / 2)
+              .attr("fill", function(d) {
+		            if  ( d.properties.indice_desigualdad == null)  {
+									return "#CCC";
+								} else {
+									return colorPriv(d.properties.perc_alum_ext_priv)
+								}
+							})
+              .attr("stroke", "#CCC")
+              .attr("stroke-width", 1)
+              .attr("rx", 0.5)
+          })
+*/
+		// Texto de zona
+		arrows.append("text")
+			.each(function(d) {
+			//var zona = ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) ? " " : d.properties.zona;
+			d3.select(this)
+				.attr("text-anchor", "left")
+				.attr("dy", 12)
+				.attr("dx", "4px")
+				.text( function(d) {
+					var punto = (d.properties.zona.length > 10)? "." : "";
+					return d.properties.zona.substring(0,10) + punto;
+					})
+				.style("fill", "black")
+				.style("font-size", "13px");
+		})
+		// texto diferencia
+    arrows.append("text")
+      .each(function(d) {
+				var zona = ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) ? " " : d.properties.zona;
+        d3.select(this)
+          .attr("text-anchor", "middle")
+		      .attr("dy", -13)
+		      .attr("dx", square/2)
+		      .text(
+		      function(d) {
+				      if ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) {
+							texto = "";
+							} else { texto = d3.format(",.0f")(d.properties.perc_bec_comedor_pub - d.properties.perc_bec_comedor_priv) + "%" }
+				      return texto;
+				      }
+		      )
+		      .style("fill", function(d) {
+						var desigualdad = (d.properties.indice_desigualdad == null ) ? 0 : d.properties.indice_desigualdad;
+						var colora = ( desigualdad > 1 ) ? "#bd0017" : "blue";
+						return colora;
+						}
+		      )
+		      .style("font-size", "13px")
+		      })
+		// % de red pública
+/*    arrows.append("text")
+      .each(function(d) {
+				var zona = ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) ? " " : d.properties.zona;
+        d3.select(this)
+          .attr("text-anchor", "left")
+		      .attr("dy", 30)
+		      .attr("dx", lineSize(d.properties.perc_alum_ext_publi) + 4)
+		      .text(d.properties.perc_alum_ext_publi + "%")
+		      .style("fill", "red")
+		      .style("font-size", "11px")
+      })
+		// % de red privada
+    arrows.append("text")
+      .each(function(d) {
+				var zona = ( d.properties.zona == "Igorre" || d.properties.zona == "Montaña alavesa" || d.properties.zona == "Basurto-Zorroza" ) ? " " : d.properties.zona;
+        d3.select(this)
+          .attr("text-anchor", "right")
+		      .attr("dy", 30)
+		      .attr("dx", lineSize(d.properties.perc_alum_ext_priv) - 12)
+		      .text(d3.format(",.0f")(d.properties.perc_alum_ext_priv))
+		      .style("fill", "blue")
+		      .style("font-size", "11px")
+      })
+*/
+
+      function showTooltip(d) {
+          // Fill the tooltip
+          var desigualdad = (d.properties.indice_desigualdad == null ) ? "--" : d.properties.indice_desigualdad;
+
+					if  ( desigualdad == "--" ) {
+						desigualdad = "--";
+						cociente = "valor nulo al no haber centros privados concertados";
+					} else if ( desigualdad > 1 ) {
+						desigualdad = d.properties.indice_desigualdad;
+						diferencia = d3.format(",.1f")(d.properties.perc_alum_ext_publi - d.properties.perc_alum_ext_priv);
+						diferencia_explica = "% pública - % privada";
+						cociente = "% pública / % privada";
+					} else {
+						desigualdad = d3.format(",.2f")(d.properties.perc_alum_ext_priv / d.properties.perc_alum_ext_publi )
+						diferencia = d3.format(",.1f")(d.properties.perc_alum_ext_priv - d.properties.perc_alum_ext_publi)
+						diferencia_explica = "% privada - % pública";
+						cociente = "% privada / % pública";
+					}
+        
+          var privado = (d.properties.perc_alum_ext_priv == null ) ? "-- " : d.properties.perc_alum_ext_priv;
+
+          tooltip.html("<div class='table-responsive'><strong>" + d.properties.zona + "</strong> (zona escolar " + d.properties.zona_id2 + ")</div>" +
+            "<table class='table table-condensed table-striped'>" +
+                "<tr>" +
+                    "<td style='text-align:right'><strong>"+ d.properties.perc_bec_comedor_pub +"%</strong></td><td>alumnado becado comedor en la red <strong>pública</strong></td>" +
+                "</tr>" +
+               "<tr>" +
+                    "<td style='text-align:right'><strong>"+ d.properties.perc_bec_comedor_priv +"%</strong></td><td>alumnado becado comedor en la red <strong>privada</strong></td>" +
+                "</tr>" +
+								"<tr>" +
+                    "<td style='text-align:right'>"+ d.properties.total_alumnado +"</td><td> alumnado</td>" +
+                "</tr>" +
+              "</table>")
+            .style("opacity", 1)
+
+          tooltip.style("left", (d3.event.pageX + 20) + "px")
+          tooltip.style("top", (d3.event.pageY + 23) + "px")
+        }
+
+      function hideTooltip(d) {
+        // Hide tooltip
+        tooltip.style("opacity", 0)
+      }
+})
+
+// From http://bl.ocks.org/mbostock/4055889
+function collide() {
+  for (var k = 0, iterations = 4, strength = 0.5; k < iterations; ++k) {
+    for (var i = 0, n = barrio.length; i < n; ++i) {
+      for (var a = barrio[i], j = i + 1; j < n; ++j) {
+        var b = barrio[j],
+            x = a.x + a.vx - b.x - b.vx,
+            y = a.y + a.vy - b.y - b.vy,
+            lx = Math.abs(x),
+            ly = Math.abs(y),
+            r = a.area/2 + b.area/2 + padding;
+        if (lx < r && ly < r) {
+          if (lx > ly) {
+            lx = (lx - r) * (x < 0 ? -strength : strength);
+            a.vx -= lx, b.vx += lx;
+          } else {
+            ly = (ly - r) * (y < 0 ? -strength : strength);
+            a.vy -= ly, b.vy += ly;
+          }
+        }
+      }
+    }
+  }
+}
